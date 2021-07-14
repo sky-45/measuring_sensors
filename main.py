dff770afc6c1bd47c81dfdc945d8489448ae511e
Johan B <ja_johan@hotmail.com>
2021-07-14 13:19:10 -0500

    minor changes, trying to implement some sort of db in memory

--- main.py
+++ main.py
@@ -2,26 +2,28 @@
 from fastapi import FastAPI
 from datetime import datetime
 from pydantic import BaseModel
+import pandas as pd
 
-
-
-measurement_adc = {"ADC":{"14/07/2021":{"11:15:10":15}}}
+measurement_adc = [{"sensor":"ADC", "date":"14/07/2021", "time":"11:15:10", "value": 15}]
+df = pd.DataFrame(measurement_adc)
 class Item(BaseModel):
     sensor: str
     value_measured_mem: int
-
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": measurement_adc}
+    #return df.to_dict("index")
+    return measurement_adc
 
 @app.post("/post_data")
 async def post_data(item: Item):
     now = datetime.now()
     day = now.strftime("%d/%m/%Y")
     hour = now.strftime("%H:%M:%S")
-    measurement_adc[item.sensor][day][hour] = item.value_measured_mem
+    #df_temp = pd.DataFrame([{"sensor":item.sensor, "date":day, "time":hour, "value": item.value_measured_mem}])
+    #df.append(df_temp, ignore_index = True)
+    measurement_adc.append({"sensor":item.sensor, "date":day, "time":hour, "value": item.value_measured_mem})
     return item
 
