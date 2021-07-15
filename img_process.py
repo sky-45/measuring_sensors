26b11e13d9f5ed5ce74053df49ccc459019a366a
Johan B <ja_johan@hotmail.com>
2021-07-14 22:47:39 -0500

    working proto

--- img_process.py
+++ img_process.py
@@ -1,4 +1,20 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-def get_plt_sensor(sensor:str, samples:int, arr_data:list):
-    return 0
+import seaborn as sns
+import os
+sns.set(rc={'figure.figsize':(11, 4)})
+def get_plt_sensor(sensor:str, last_samples:int, arr_data:list):
+    path = 'media/test.png'
+    #os.remove(path)
+    df = pd.DataFrame(arr_data)
+    df["datetime"]=pd.to_datetime(df["datetime"])
+    df = df.set_index('datetime')
+    plt.plot(df['value'].sort_index().tail(last_samples))
+    plt.xlabel('Fecha y Hora')
+    plt.ylabel('Valor medido')
+    plt.title("Medidas sensor")
+    plt.savefig(path)
+    plt.close()
+    return path
+    
+
--- main.py
+++ main.py
@@ -3,8 +3,21 @@ from fastapi import FastAPI
 from datetime import datetime
 from pydantic import BaseModel
 import pandas as pd
+from fastapi.responses import FileResponse
+import random
+from img_process import *
 
-measurement_adc = [{"sensor":"ADC", "date":"14/07/2021", "time":"11:15:10", "value": 15}]
+measurement_adc = [{"sensor":"ADC", "datetime":"14/07/2021 11:15:10", "value": 5},
+                  {"sensor":"ADC", "datetime":"14/07/2021 11:17:10", "value": 2},
+                  {"sensor":"ADC", "datetime":"14/07/2021 11:19:10", "value": 4},
+                  {"sensor":"ADC", "datetime":"14/07/2021 11:20:10", "value": 6},
+                  {"sensor":"ADC", "datetime":"14/07/2021 11:21:10", "value": 4},
+                  {"sensor":"ADC", "datetime":"14/07/2021 11:22:10", "value": 7}
+                  ]
+for k in range(1,600):
+    temp = {"sensor":"ADC", "datetime":"14/07/2021 11:{}:{}".format(k%50, random.randint(2, 55)), "value": 5+random.random()}
+    measurement_adc.append(temp)
+
 df = pd.DataFrame(measurement_adc)
 class Item(BaseModel):
     sensor: str
@@ -22,17 +35,12 @@ async def post_data(item: Item):
     now = datetime.now()
     day = now.strftime("%d/%m/%Y")
     hour = now.strftime("%H:%M:%S")
-    #df_temp = pd.DataFrame([{"sensor":item.sensor, "date":day, "time":hour, "value": item.value_measured_mem}])
-    #df.append(df_temp, ignore_index = True)
-    measurement_adc.append({"sensor":item.sensor, "date":day, "time":hour, "value": item.value_measured_mem})
-    #return item
+    measurement_adc.append({"sensor":item.sensor, "datetime":day+' '+hour, "value": item.value_measured_mem})
+    return item
 
-@app.get("/aclr")   #acelerometro
-async def root():
-    return measurement_adc
-
-@app.get("/temperature")   #temperature
-async def root():
-    return measurement_adc
+@app.get("/temperature/{last_num}")   #temperature
+async def fig_temp(last_num:int):
+    pth_img=get_plt_sensor("ADC", last_num, measurement_adc)
+    return FileResponse(pth_img)
 
 
